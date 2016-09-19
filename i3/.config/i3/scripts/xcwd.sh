6882e56e5a643100f376fe4dc69d8fa5d2026d6b
Louis Jencka <louis@jencka.us>
2016-09-19 13:34:01 -0600

    Create CWD wrapper for termite

---
+++ i3/.config/i3/scripts/xcwd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# get focused window PID
+X_PID=$(xdotool getwindowfocus getwindowpid)
+
+# check if focused window isn't term
+X_CMD=$(cat /proc/"$X_PID"/cmdline)
+if ! [[ "$X_PID" ]] || [[ "$X_CMD" != "termite" ]]; then
+    # then open term normally
+    termite
+fi
+
+# get current term cwd
+SHELL_PID=/proc/$(cut -d ' ' -f 1 < /proc/"$X_PID"/task/"$X_PID"/children)/cwd
+SHELL_CWD=$(readlink "$SHELL_PID")
+
+# open new term with same cwd
+termite -d "$SHELL_CWD"
