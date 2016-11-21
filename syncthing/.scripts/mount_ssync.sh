9e7fe98149a5ea22b390f0ca170d0c83812710da
Louis Jencka <louis@jencka.us>
2016-11-21 16:13:54 -0700

    Disable sync-folder idling

--- syncthing/.scripts/mount_ssync.sh
+++ syncthing/.scripts/mount_ssync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if ! mountpoint -q /home/louis/sync; then
-    pass me/syncthing/ssync | encfs --idle=5 --stdinpass /home/louis/.ssync /home/louis/sync
+    pass me/syncthing/ssync | encfs --stdinpass /home/louis/.ssync /home/louis/sync
 fi
